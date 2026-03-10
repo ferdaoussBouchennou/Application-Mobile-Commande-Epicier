be202361f4bc94dee421490726e1df286a141ee4
ferdaoussBouchennou <ferdaousbo12@gmail.com>
2026-03-10 06:08:32 +0000

    test en chrome

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,12 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+    contentSecurityPolicy: false, // Optionnel, mais aide en dev web
+  })
+);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +27,7 @@ app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: 'API is running 🚀', env: process.env.NODE_ENV || 'development' });
+  res.json({ message: 'API is running ', env: process.env.NODE_ENV || 'development' });
 });
 
 // Routes principales de l'API
