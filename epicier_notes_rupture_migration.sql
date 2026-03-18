e3cd48ef8f2c71582488f87b18c02bb0290127d8
tafraouti-sanae01 <tafraouti.sanae1@gmail.com>
2026-03-18 21:15:59 +0000

    refactor(db): fusionner les 3 migrations en epicier_migration_complete.sql

--- epicier_notes_rupture_migration.sql
+++
@@ -1,6 +0,0 @@
--- Migration: notes commande + rupture par ligne + acceptation client
--- À exécuter sur epicier_ecommerce
-
-ALTER TABLE commandes ADD COLUMN notes TEXT DEFAULT NULL;
-ALTER TABLE detailscommande ADD COLUMN rupture TINYINT(1) DEFAULT 0;
-ALTER TABLE commandes ADD COLUMN client_accepte_modification TINYINT(1) DEFAULT 0;
