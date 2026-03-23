c2ffdbc748a7ffb4c27a6715cfae79b5a8df0015
tafraouti-sanae01 <tafraouti.sanae1@gmail.com>
2026-03-23 15:43:46 +0100

    sql: dump unique et script de generation

--- update_reclamations_status_enum.sql
+++
@@ -1,9 +0,0 @@
--- Normalize legacy statuses to the new allowed set.
-UPDATE reclamations SET statut = 'En attente' WHERE statut IN ('Ouverte');
-UPDATE reclamations SET statut = 'En médiation' WHERE statut IN ('En cours');
-UPDATE reclamations SET statut = 'Résolu' WHERE statut IN ('Résolue');
-
--- Modify enum to the new allowed set.
-ALTER TABLE reclamations
-  MODIFY statut ENUM('En attente','Résolu','En médiation','Remboursé','Litige ouvert')
-  NOT NULL;
