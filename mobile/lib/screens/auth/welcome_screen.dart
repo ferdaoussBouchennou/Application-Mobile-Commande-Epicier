1d1cfa651c575ade70585ef28b2a2f8c317f2639
zakariyae <allouchezaki45@gmail.com>
2026-03-24 01:59:05 +0100

    logo in welcome screen

--- mobile/lib/screens/auth/welcome_screen.dart
+++ mobile/lib/screens/auth/welcome_screen.dart
@@ -75,7 +75,7 @@ class WelcomeScreen extends StatelessWidget {
   
                 // Logo central
                 Image.asset(
-                  'assets/images/loading.png',
+                  'assets/images/logo.png',
                   height: 220,
                   errorBuilder: (context, error, stackTrace) => const Icon(Icons.shopping_basket, color: Color(0xFF2D5016), size: 120),
                 ),
